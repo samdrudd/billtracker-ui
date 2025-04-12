77feff9ad3e060314c55c1a682493ada32506db2
Sam Rudd <samdrudd@gmail.com>
2025-04-12 11:42:03 -0700

    Add config to easily switch API urls between dev/prod

---
+++ api.js
@@ -0,0 +1,3 @@
+const API = {
+    URL: (config.ENV === ENV_DEV) ? 'http://localhost:8000/' : 'https://billtracker-api.onrender.com/'
+};
---
+++ config.js
@@ -0,0 +1,6 @@
+const ENV_DEV = 0;
+const ENV_PROD = 1;
+
+const config = {
+    ENV: ENV_PROD
+};
