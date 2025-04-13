8265f01feb25178a3ffce2bbe42e0dbf86b0d85e
Sam Rudd <samdrudd@gmail.com>
2025-04-13 11:54:43 -0700

    Move transaction logic with transaction class

--- transaction.js
+++ transaction.js
@@ -1,4 +1,6 @@
 class Transaction {
+    static route = API.URL + 'transactions';
+
     constructor(id, name, amount, repeats, repeatsOn, date) {
         this.id = id;
         this.amount = amount;
@@ -16,6 +18,12 @@ class Transaction {
 
     displayDate() {
         return moment(this.date).format("M-D-YYYY");
+    }
+
+    isDateValid() {
+        var currentDate = new Date();
+        var transactionDate = new Date(this.date);
+        return transactionDate.getMonth() === currentDate.getMonth() && transactionDate >= currentDate;
     }
 
     html() {
@@ -39,12 +47,10 @@ class Transaction {
             Date: this.startsOn
         };
     }
-}
 
-const TransactionController = {
-    getAll: function(successCallback, errorCallback) {
+    static getAll(successCallback, errorCallback) {
         $.ajax({
-            url: API.URL + 'transactions',
+            url: Transaction.route,
             method: 'GET',
             success: function(data) {
                 // id, name, amount, repeats, repeatsOn, date
@@ -66,5 +72,63 @@ const TransactionController = {
                 errorCallback(error);
             }
         });
+    }
+
+    static processTransactions(transactions) {
+        // Update transaction dates for current month and year, filter out past transactions and those not in the current month, sort by date
+        var processedTransactions = [];
+
+        transactions.forEach(function(transaction) {
+            // If the transaction repeats weekly, determine which dates will occur within the current month            
+            if (transaction.repeats === "Weekly") {
+                var currentDate = new Date();
+                var lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+                var startDate = new Date(transaction.date);
+                var repeatDay = startDate.getDay();
+                var repeatInterval = parseInt(transaction.repeatsOn);
+
+                var dates = [];
+
+                var intervalDate = startDate;
+                while (intervalDate <= lastDayOfMonth) {
+                    if (intervalDate >= currentDate) {
+                        dates.push(intervalDate.toISOString().split('T')[0]);
+                    }
+                    intervalDate.setDate(intervalDate.getDate() + repeatInterval * 7);
+                }
+
+                // If valid dates, add them to the processed transactions to be returned
+                if (dates != []) {
+                    // Set the date of the current transaction object to the first valid date (keep the starting date)
+                    var startingDate = transaction.date;
+
+                    // If there are multiple valid dates, create a new transaction object for each date
+                    for (var i = 0; i < dates.length; i++) {
+                        var newTransaction = new Transaction(transaction.id, transaction.name, transaction.amount, transaction.repeats, transaction.repeatsOn, dates[i]);
+                        newTransaction.startsOn = startingDate; // Set the starting date to the original transaction date
+                        
+                        // If the date for the transaction falls within the current month and is not in the past, add it to the processed transactions to be returned
+                        if (newTransaction.isDateValid())
+                            processedTransactions.push(newTransaction);
+                    }
+                }
+            }
+
+            // If the transaction is monthly, update the date to the corresponding day of the current month
+            if (transaction.repeats === "Monthly") {
+                transaction.startsOn = transaction.date;
+                transaction.date = new Date(new Date().getFullYear(), new Date().getMonth(), transaction.repeatsOn);
+
+                // If the date for the transaction falls within the current month and is not in the past, add it to the processed transactions to be returned
+                if (transaction.isDateValid())
+                    processedTransactions.push(transaction);
+            }
+        });
+        
+        processedTransactions.sort(function(a, b) {
+            return new Date(a.date) - new Date(b.date);
+        });
+
+        return processedTransactions;
     }
 }
